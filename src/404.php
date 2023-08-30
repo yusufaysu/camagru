4a3f41816feca70b20d558a9a633dad3011241ca
yaysu <spidermancuf01@gmail.com>
2023-08-30 11:09:33 +0000

    404 not found added

--- src/404.php
+++ src/404.php
@@ -1,1 +1,24 @@
-yarraaaaaaaaaaaaa
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="./styles/header.css">
+        <link rel="stylesheet" href="./styles/404.css">
+        <title>Camagru</title>
+    </head>
+    <body style='background: #29372e'>
+        <div class="block" style="  display: flex;
+                                    justify-content: center;
+                                    padding: 100px;">
+            <div class="alert" style="  display: block;
+                                        text-align: center;
+                                        color: bisque;">
+                <h1>404 NOT FOUND</h1>
+                <h1>Sorry, this page isn't available.</h1>
+                <p1>The link you followed may be broken, or the page may have been removed. Go back to <a href="/" style="color: bisque;">Camagru.</a></p1>
+            </div>
+        </div>
+
+    </body>
+</html>
+
